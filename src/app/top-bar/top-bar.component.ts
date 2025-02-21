eac011e2223139110b5fe78b777f76f97f748e6e
gourav310 <git@stackblitz.com>
2025-02-21 14:02:17 +0530

    Initial commit

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css'],
+})
+export class TopBarComponent {
+  count = 0;
+  increament() {
+    this.count++;
+  }
+  decrement() {
+    this.count--;
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
